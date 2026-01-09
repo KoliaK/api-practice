6c7dadec742f72080b3aaa75955ca847006d1b04
KoliaK <xkoliakx@gmail.com>
2026-01-09 05:06:24 -0300

    freeze requirements.txt and created 1.1the_hello_world.py

---
+++ src/1.1the_hello_world.py
@@ -0,0 +1,14 @@
+# SINGLE ITEM FETCH
+'''
+endpoint : 
+https://jsonplaceholder.typicode.com/todos/1
+
+action:
+perform a GET request to fetch the data
+
+Output: 
+parse the response and print ONLY the value of the "title" field.
+'''
+
+import requests
+
